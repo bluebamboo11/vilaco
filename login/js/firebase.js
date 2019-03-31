0551b9b8a71ded2cc7a321ead808a8f81f024232
tongthanhtung <bluebamboo195@gmail.com>
2019-03-31 19:00:29 +0700

    Initial commit

---
+++ login/js/firebase.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyAWVXjquspKBvxwhZo9EIzLYav9mEkJHw4",
+    authDomain: "vinet-77117.firebaseapp.com",
+    databaseURL: "https://vinet-77117.firebaseio.com",
+    projectId: "vinet-77117",
+    storageBucket: "vinet-77117.appspot.com",
+    messagingSenderId: "233967204421"
+};
+firebase.initializeApp(config);
+
+function login(userName,pass) {
+
+}
+
